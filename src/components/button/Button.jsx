73e6e0dbe3704bb1ec0a4b27f612beccb127d9ae
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-06-05 21:27:05 +0200

    Github link need styling

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+
+const Button = () => {
+  return (
+    <>
+      <div className="button-container">
+        <a href="olekorvald.no" className="cross-fade">
+          GitHub
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Button;
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,4 +1,6 @@
 import firstWork from "../../images/bergen.jpg";
+import Button from "../button/Button";
+import { AiFillGithub } from "react-icons/ai";
 
 const Works = () => {
   return (
@@ -8,12 +10,27 @@ const Works = () => {
 
         <div className="work-wrap">
           <div className="workContainerOne">
-            <h2>Prosjekt for en hifi butikk</h2>
+            <h2>Hifi-Club</h2>
             <div className="cardOne">
               <div className="imageContainerOne">
                 <img src={firstWork} alt="" className="workImages" />
+              </div>{" "}
+              <div className="WorkDescription">
+                <p>
+                  Hifi-Club er en tenkt nettside til en butikk som selger
+                  høyttalere og tv-er. Kvalitetene er av høy standard og er ofte
+                  tenkt til de som virkelig er interessert i lyd og bilde, samt
+                  er villig til å betale litt ekstra for det.
+                  <br /> <br /> Dette var en skoleoppgave vi fikk gjøre før jul
+                  2021, og vi hadde 5 uker på oss til å levere.
+                </p>
               </div>
-            </div>
+              <div className="workButton">
+                {" "}
+                <Button className="buttonComponent" />
+                <AiFillGithub />
+              </div>
+            </div>{" "}
           </div>
           <div className="workContainerOne">
             <h2>skoleoppgave</h2>
@@ -21,13 +38,39 @@ const Works = () => {
               <div className="imageContainerOne">
                 <img src={firstWork} alt="" className="workImages" />
               </div>
+              <div className="WorkDescription">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Eligendi asperiores libero nam cumque rerum mollitia dolores
+                  assumenda eveniet at odio. Obcaecati deserunt eaque quidem
+                  omnis nobis harum quae quod eveniet!
+                </p>
+              </div>
+              <div className="workButton">
+                <Button />
+              </div>
             </div>
           </div>
           <div className="workContainerOne">
             <h2>Hotell booking side</h2>
             <div className="cardOne">
+              {""}
               <div className="imageContainerOne">
                 <img src={firstWork} alt="" className="workImages" />
+              </div>
+              {""}
+
+              <div className="WorkDescription">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Eligendi asperiores libero nam cumque rerum mollitia dolores
+                  assumenda eveniet at odio. Obcaecati deserunt eaque quidem
+                  omnis nobis harum quae quod eveniet!
+                </p>
+              </div>
+
+              <div className="workButton">
+                <Button />
               </div>
             </div>
           </div>
