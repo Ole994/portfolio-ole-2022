affb83cfc7e507724fe525cb62f78949d43ccb98
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-06-08 19:55:42 +0200

    hamburger in progress

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,154 +9,99 @@ import {
 import { BsFillPersonFill, BsInstagram } from "react-icons/bs";
 import { MdWorkspaces } from "react-icons/md";
 import { FaFacebook } from "react-icons/fa";
+import { useState } from "react";
 
 import { GiHamburgerMenu } from "react-icons/gi";
 
 //Mobile
 
 const Nav = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+  // const [auth, setAuth] = useContext(AuthContext);
+  // const navigate = useNavigate();
   return (
     <>
       {" "}
-      <div className="navigation-mobile">
-        <ul>
-          {" "}
-          <li>
-            <a href="#homeId">
-              <span className="icon">
-                <AiFillHome></AiFillHome>
-              </span>
-              <span className="title">Fremside</span>
-            </a>
-          </li>
-          <li>
-            <a href="#worksId">
-              <span className="icon">
-                <MdWorkspaces />
-              </span>
-              <span className="title">Mine prosjekter</span>
-            </a>
-          </li>
-          <li>
-            <a href="#aboutMeId">
-              <span className="icon">
-                <BsFillPersonFill />
-              </span>
-              <span className="title">Om meg</span>
-            </a>
-          </li>
-          <li>
-            <a href="#contactId">
-              <span className="icon">
-                <AiOutlineMail />
-              </span>
-              <span className="title">Contact</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/ole994/">
-              <span className="icon">
-                <FaFacebook />
-              </span>
-              <span className="title"> Facebook</span>
-            </a>
-          </li>
-          <li>
-            <Link to="/">
-              <span className="icon">
-                <AiFillGithub></AiFillGithub>
-              </span>
-              <span className="title">Github</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <span className="icon">
-                <AiFillLinkedin></AiFillLinkedin>
-              </span>
-              <span className="title">LinkedIn</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <span className="icon">
-                <BsInstagram></BsInstagram>
-              </span>
-              <span className="title">Instagram</span>
-            </Link>
-          </li>
-        </ul>
-      </div>{" "}
-      {/* desktop */}
+      {/* desktop */}{" "}
+      <GiHamburgerMenu
+        className="hamburger-icon"
+        onClick={() => {
+          console.debug("helllooo");
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      />
       <div className="navigation">
-        <ul>
-          <li>
-            <a href="#homeId">
-              <span className="icon">
-                <AiFillHome></AiFillHome>
-              </span>
-              <span className="title">Fremside</span>
-            </a>
-          </li>
+        {isNavExpanded && (
+          <ul>
+            <li>
+              <a href="#homeId">
+                <span className="icon">
+                  <AiFillHome></AiFillHome>
+                </span>
+                <span className="title">Fremside</span>
+              </a>
+            </li>
 
-          <li>
-            <a href="#worksId">
-              <span className="icon">
-                <MdWorkspaces />
-              </span>
-              <span className="title">Mitt arbeid</span>
-            </a>
-          </li>
-          <li>
-            <a href="#aboutMeId">
-              <span className="icon">
-                <BsFillPersonFill />
-              </span>
-              <span className="title">Om meg</span>
-            </a>
-          </li>
-          <li>
-            <a href="#contactId">
-              <span className="icon">
-                <AiOutlineMail />
-              </span>
-              <span className="title">Contact</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/ole994/">
-              <span className="icon">
-                <FaFacebook />
-              </span>
-              <span className="title">Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/Ole994">
-              <span className="icon">
-                <AiFillGithub></AiFillGithub>
-              </span>
-              <span className="title">Github</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/ole-korvald/">
-              <span className="icon">
-                <AiFillLinkedin></AiFillLinkedin>
-              </span>
-              <span className="title">LinkedIn</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/ole994/">
-              <span className="icon">
-                <BsInstagram></BsInstagram>
-              </span>
-              <span className="title">Instagram</span>
-            </a>
-          </li>
-        </ul>
+            <li>
+              <a href="#worksId">
+                <span className="icon">
+                  <MdWorkspaces />
+                </span>
+                <span className="title">Mitt arbeid</span>
+              </a>
+            </li>
+            <li>
+              <a href="#aboutMeId">
+                <span className="icon">
+                  <BsFillPersonFill />
+                </span>
+                <span className="title">Om meg</span>
+              </a>
+            </li>
+            <li>
+              <a href="#contactId">
+                <span className="icon">
+                  <AiOutlineMail />
+                </span>
+                <span className="title">Contact</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/ole994/">
+                <span className="icon">
+                  <FaFacebook />
+                </span>
+                <span className="title">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/Ole994">
+                <span className="icon">
+                  <AiFillGithub></AiFillGithub>
+                </span>
+                <span className="title">Github</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/ole-korvald/">
+                <span className="icon">
+                  <AiFillLinkedin></AiFillLinkedin>
+                </span>
+                <span className="title">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/ole994/">
+                <span className="icon">
+                  <BsInstagram></BsInstagram>
+                </span>
+                <span className="title">Instagram</span>
+              </a>
+            </li>
+          </ul>
+        )}
       </div>{" "}
+      )
     </>
   );
 };
